758f287a8d512e5f09544211dfbcc170363f23e0
shigeyuki.fujishima <shigeyuki.fujishima@gmail.com>
2020-02-13 01:22:43 +0900

    Solved module01/num_in_list

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -1,7 +1,35 @@
 package module01
 
+import (
+	"sort"
+)
+
 // NumInList will return true if num is in the
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
+	if len(list) == 0 {
+		return false
+	}
+
+	sort.Ints(list)
+
+	for {
+		idx := len(list) / 2
+
+		if list[idx] == num {
+			return true
+		}
+
+		if len(list) == 1 {
+			break
+		}
+
+		if list[idx] > num {
+			list = list[:len(list)/2]
+		} else {
+			list = list[len(list)/2:]
+		}
+	}
+
 	return false
 }
